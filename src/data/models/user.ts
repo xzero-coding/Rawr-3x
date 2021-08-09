1de87269d2ab7dd8d2304192f79877e9087d1471
xzero-coding <83907241+xzero-coding@users.noreply.github.com>
2021-08-09 22:31:47 +0200

    Update user.ts

--- src/data/models/user.ts
+++ src/data/models/user.ts
@@ -20,7 +20,7 @@ export interface UserDocument extends Document {
 export const SavedUser = model<UserDocument>('user', new Schema({
   _id: String,
   guildPositions: { type: Array, default: [] },
-  premium: { type: Boolean, default: false },
+  premium: { type: Boolean, default: true },
   premiumExpiration: { type: Date, default: null },
   votes: { type: Number, default: 0 },
   xpCard: { type: Object, default: new XPCard() },
